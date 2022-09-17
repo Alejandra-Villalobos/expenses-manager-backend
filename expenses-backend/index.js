19cce06ed95acf52671e335bc41a3ef1bc43d6dc
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-09-16 23:01:34 -0600

    Added: Routes to index

--- expenses-backend/index.js
+++ expenses-backend/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
+const oracle = require('./src/utils/db');
 const { server } = require('./src/config/config');
+const guard = require('./src/guard/guard');
 
-const invalidRoutes = require('./src/routes/notFound');
+const personRoutes = require('./src/routes/person');
 const bankRoutes = require('./src/routes/bank');
+const incomeRoutes = require('./src/routes/income');
+const outcomeRoutes = require('./src/routes/outcome');
+const invalidRoutes = require('./src/routes/notFound');
 
 app.use(express.json());
 
+app.use(personRoutes);
+app.use(guard);
 app.use(bankRoutes);
+app.use(incomeRoutes);
+app.use(outcomeRoutes);
 app.use(invalidRoutes);
 
-app.listen(server.port, () => {
-    console.log(`Server running on port: ${server.port}`)
-});
+oracle
+  .start()
+  .then(() => {
+    console.log('Oracle database connected!');
+    app.listen(server.port, () => {
+      console.log(`Server running on port: ${server.port}`);
+    });
+  })
+  .catch((error) => console.log(error));
