814e07799b01648cedba0edc5bf8972036befae9
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-09-16 22:24:34 -0600

    Fixed: index route

--- expenses-backend/index.js
+++ expenses-backend/index.js
@@ -3,11 +3,11 @@ const app = express();
 const { server } = require('./src/config/config');
 
 const invalidRoutes = require('./src/routes/notFound');
-const categoryRoutes = require('./src/routes/category');
+const bankRoutes = require('./src/routes/bank');
 
 app.use(express.json());
 
-app.use(categoryRoutes);
+app.use(bankRoutes);
 app.use(invalidRoutes);
 
 app.listen(server.port, () => {
